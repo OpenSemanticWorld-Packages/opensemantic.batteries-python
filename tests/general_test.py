ae5cf656d2cd5278091c2adb7367a69d60656be1
SimonTaurus <simon.stier@gmx.de>
2026-06-29 16:14:58 +0200

    Add generated battery models (schema v1.2.1)

--- tests/general_test.py
+++ tests/general_test.py
@@ -1,8 +1,34 @@
-def test_import():
-    import opensemantic.batteries  # noqa: F401
-    import opensemantic.batteries.v1  # noqa: F401
+import opensemantic.batteries
+import opensemantic.batteries.v1
+import opensemantic.core
+import opensemantic.core.v1
+
+
+def test_opensemantic():
+
+    # Create an instance of BatteryCell
+    model = opensemantic.batteries.BatteryCell(
+        label=[opensemantic.core.Label(text="Test Entity")],
+    )
+
+    # Check if the instance is created successfully
+    assert isinstance(
+        model, opensemantic.batteries.BatteryCell
+    ), "Failed to create an instance of BatteryCell"
+
+    # v1 tests
+
+    # Create an instance of BatteryCell
+    model = opensemantic.batteries.v1.BatteryCell(
+        label=[opensemantic.core.v1.Label(text="Test Entity")],
+    )
+
+    # Check if the instance is created successfully
+    assert isinstance(
+        model, opensemantic.batteries.v1.BatteryCell
+    ), "Failed to create an instance of BatteryCell"
 
 
 if __name__ == "__main__":
-    test_import()
+    test_opensemantic()
     print("All tests passed!")
